03932e2f9e809ae0cdf4fb90ac5f4336168d6669
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-12-03 08:46:06 +0100

    2024 day 3

---
+++ src/main/kotlin/year2024/Day3.kt
@@ -0,0 +1,44 @@
+package year2024
+
+import me.koendev.*
+
+fun main() {
+    part1(getInput(2024, 3)).println()
+    part2(getInput(2024, 3)).println()
+}
+
+
+private fun part1(input: List<String>): Int {
+    var sum = 0
+
+    val regex = Regex("mul\\(\\d+,\\d+\\)")
+    for (line in input) {
+        for (match in regex.findAll(line)) {
+            val splits = match.value.split("(",")",",")
+            sum += splits[1].toInt() * splits[2].toInt()
+        }
+    }
+
+    return sum
+}
+
+
+private fun part2(input: List<String>): Int {
+    var sum = 0
+    var enabled = true
+
+    val regex = Regex("mul\\(\\d+,\\d+\\)|don't\\(\\)|do\\(\\)")
+    for (line in input) {
+        for (match in regex.findAll(line)) {
+            if (match.value == "don't()") enabled = false
+            else if (match.value == "do()") enabled = true
+
+            else if (enabled) {
+                val splits = match.value.split("(", ")", ",")
+                sum += splits[1].toInt() * splits[2].toInt()
+            }
+        }
+    }
+
+    return sum
+}
