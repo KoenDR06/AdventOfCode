d436fcee5ce790a01a9ee8747f3862fe9c38e86c
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-12-05 10:11:51 +0100

    2024 day 5

---
+++ src/main/kotlin/year2024/Day5.kt
@@ -0,0 +1,76 @@
+package year2024
+
+import me.koendev.*
+
+fun main() {
+    part1(getInput(2024, 5)).println()
+    part2(getInput(2024, 5)).println()
+}
+
+
+private fun part1(input: List<String>): Int {
+    val rules: MutableList<Pair<Int, Int>> = mutableListOf()
+    var sum = 0
+    var writingRules = true
+    for (line in input) {
+        if (line == "") {
+            writingRules = false
+            continue
+        }
+
+        if (writingRules) {
+            val splits = line.split("|").map { it.toInt() }
+
+            rules.add(Pair(splits[0], splits[1]))
+            continue
+        }
+
+        val pages = line.split(",").map { it.toInt() }
+        if (rules.all {
+            if (pages.indexOf(it.first) == -1 || pages.indexOf(it.second) == -1) true
+            else pages.indexOf(it.first) < pages.indexOf(it.second)
+        }) sum += pages[(pages.size - 1) / 2]
+    }
+
+    return sum
+}
+
+
+private fun part2(input: List<String>): Int {
+    val rules: MutableList<Pair<Int, Int>> = mutableListOf()
+    var sum = 0
+    var writingRules = true
+    for (line in input) {
+        if (line == "") {
+            writingRules = false
+            continue
+        }
+
+        if (writingRules) {
+            val splits = line.split("|").map { it.toInt() }
+
+            rules.add(Pair(splits[0], splits[1]))
+            continue
+        }
+
+        val pages = line.split(",").map { it.toInt() }.toMutableList()
+        var changeMade = false
+        while (!rules.all { rule ->
+                if (pages.indexOf(rule.first) == -1 || pages.indexOf(rule.second) == -1) true
+                else pages.indexOf(rule.first) < pages.indexOf(rule.second)
+            }) {
+            for (rule in rules) {
+                if (pages.indexOf(rule.first) == -1 || pages.indexOf(rule.second) == -1) continue
+
+                if (pages.indexOf(rule.first) > pages.indexOf(rule.second)) {
+                    val temp = pages.removeAt(pages.indexOf(rule.second))
+                    pages.add(temp)
+                    changeMade = true
+                }
+            }
+        }
+        if (changeMade) sum += pages[(pages.size - 1) / 2]
+    }
+
+    return sum
+}
