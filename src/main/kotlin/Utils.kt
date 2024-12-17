a523aa5c13a792c775ffc52955f7c9351deda12e
KoenDR06 <koen.de.ruiter@hotmail.com>
2024-12-17 14:33:38 +0100

    Formalized Grid class

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -78,32 +78,42 @@ data class MutableTriple<A, B, C>(
 
 )
 
-// Grid Utils
 @Suppress("Unused")
-fun List<String>.get(x: Int, y: Int) = this[y][x]
+class Grid<T>(input: List<List<T>>) {
+    private var grid: MutableList<MutableList<T>> = mutableListOf()
 
-@Suppress("Unused")
-fun List<String>.getColumn(col: Int): String {
-    var res = ""
-
-    for (line in this) {
-        res += line[col]
-    }
-
-    return res
-}
-
-@Suppress("Unused")
-fun List<String>.transpose(): List<String> {
-    if (!this.all { it.length == this[0].length }) throw IllegalArgumentException("Input is not a grid.")
-
-    val res = MutableList(this[0].length) { "" }
-    for (line in this) {
-        for ((x, char) in line.withIndex()) {
-            res[x] = res[x] + char
+    init {
+        for (line in input) {
+            grid.add(mutableListOf())
+            for (char in line) {
+                grid.last().add(char)
+            }
         }
     }
-    return res
+
+    fun get(x: Int, y: Int): T {
+        if (x < 0 || x > grid[0].size-1) throw IllegalArgumentException("X coordinate $x out of bounds for size ${grid[0].size}")
+        if (y < 0 || y > grid.size-1) throw IllegalArgumentException("Y coordinate $y out of bounds for size ${grid.size}")
+        return grid[y][x]
+    }
+
+    fun getRow(y: Int): List<T> {
+        if (y < 0 || y > grid.size-1) throw IllegalArgumentException("Y coordinate $y out of bounds for size ${grid.size}")
+
+        return grid[y]
+    }
+
+    fun getColumn(x: Int): List<T> {
+        if (x < 0 || x > grid[0].size-1) throw IllegalArgumentException("X coordinate $x out of bounds for size ${grid[0].size}")
+
+        val res = mutableListOf<T>()
+
+        for (row in grid) {
+            res.add(row[x])
+        }
+
+        return res
+    }
 }
 
 @Suppress("Unused")
